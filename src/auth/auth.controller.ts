3a3958a46d17d0a56d41a720705bcbc561b55860
Marcos Hernandez <maalhequi95@gmail.com>
2025-03-14 10:36:33 -0500

    enable rpc filter exception

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,26 +1,59 @@
-import { Controller, Post, Put } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+import { envs } from 'src/configuration';
+import { ClientProxy } from '@nestjs/microservices';
+import { BadRequestException, Body, Controller, Inject, Post, Put } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
-  constructor() {}
+  constructor(
+    @Inject(envs.auth_services_name) private readonly authClient: ClientProxy
+  ) {}
 
   @Post('/sign-up')
-  async doSignUp() {
-    return '123';
+  async doSignUp(@Body() body) {
+    try {
+      const user = await firstValueFrom(
+        this.authClient.send({ cmd: 'do_sign_up'}, body),
+      );
+      return user;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 
   @Post('/sign-in')
-  async doSignIn() {
-    return '123456';
+  async doSignIn(@Body() body) {
+    try {
+      const user = await firstValueFrom(
+        this.authClient.send({ cmd: 'do_sign_in'}, body),
+      );
+      return user;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 
   @Post('/recovery-password')
-  async doRecoveryPassword() {
-    return '';
+  async doRecoveryPassword(@Body() body) {
+    try {
+      const user = await firstValueFrom(
+        this.authClient.send({ cmd: 'do_recovery_password'}, body),
+      );
+      return user;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 
   @Put('/change-password')
-  async doChangePassword() {
-    return '';
+  async doChangePassword(@Body() body) {
+    try {
+      const user = await firstValueFrom(
+        this.authClient.send({ cmd: 'do_change_password'}, body),
+      );
+      return user;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 }
