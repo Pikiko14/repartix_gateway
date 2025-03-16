348e499480bf799a3a60f5b962f8204c53353115
Marcos Hernandez <maalhequi95@gmail.com>
2025-03-16 10:31:40 -0500

    enable find all plans

--- src/plans/plans.controller.ts
+++ src/plans/plans.controller.ts
@@ -1,20 +1,18 @@
-import {
-  ClientProxy,
-  Payload,
-  RpcException,
-} from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 import { envs } from 'src/configuration';
 import { CreatePlanDto } from './dto/create-plan.dto';
 import { AuthGuard } from 'src/commons/guards/auth.guard';
 import { ScopesGuard } from 'src/commons/guards/scopes.guard';
 import { Scopes } from 'src/commons/decorators/scope.decorator';
-import { Controller, Inject, Post, UseGuards } from '@nestjs/common';
+import { Controller, Inject, Post, Get, UseGuards, Query } from '@nestjs/common';
+import { ClientProxy, Payload, RpcException } from '@nestjs/microservices';
+import { PaginationDto } from 'src/commons/dto/pagination.dto';
 
 @Controller('plans')
 export class PlansController {
   constructor(
-    @Inject(envs.plan_service_service) private readonly plansClient: ClientProxy,
+    @Inject(envs.plan_service_service)
+    private readonly plansClient: ClientProxy,
   ) {}
 
   @Post('/')
@@ -24,6 +22,18 @@ export class PlansController {
     try {
       const plan = await firstValueFrom(
         this.plansClient.send({ cmd: 'createPlan' }, createPlanDto),
+      );
+      return plan;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Get('/')
+  async findAll(@Query() paginationDto: PaginationDto) {
+    try {
+      const plan = await firstValueFrom(
+        this.plansClient.send({ cmd: 'findAllPlans' }, paginationDto),
       );
       return plan;
     } catch (error) {
