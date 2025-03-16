967989a48763b5342f7ba68bcb603cf59de3d422
Marcos Hernandez <maalhequi95@gmail.com>
2025-03-16 10:51:25 -0500

    enable delete plan

--- src/plans/plans.controller.ts
+++ src/plans/plans.controller.ts
@@ -1,12 +1,22 @@
+import {
+  Controller,
+  Inject,
+  Post,
+  Get,
+  UseGuards,
+  Query,
+  Delete,
+  Param,
+} from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
 import { envs } from 'src/configuration';
 import { CreatePlanDto } from './dto/create-plan.dto';
 import { AuthGuard } from 'src/commons/guards/auth.guard';
 import { ScopesGuard } from 'src/commons/guards/scopes.guard';
-import { Scopes } from 'src/commons/decorators/scope.decorator';
-import { Controller, Inject, Post, Get, UseGuards, Query } from '@nestjs/common';
-import { ClientProxy, Payload, RpcException } from '@nestjs/microservices';
 import { PaginationDto } from 'src/commons/dto/pagination.dto';
+import { Scopes } from 'src/commons/decorators/scope.decorator';
+import { ClientProxy, Payload, RpcException } from '@nestjs/microservices';
+import { PlanIdDto } from './dto/pland-id.dto';
 
 @Controller('plans')
 export class PlansController {
@@ -16,7 +26,7 @@ export class PlansController {
   ) {}
 
   @Post('/')
-  @Scopes('create-plans')
+  @Scopes('create-plan')
   @UseGuards(AuthGuard, ScopesGuard)
   async create(@Payload() createPlanDto: CreatePlanDto) {
     try {
@@ -34,6 +44,20 @@ export class PlansController {
     try {
       const plan = await firstValueFrom(
         this.plansClient.send({ cmd: 'findAllPlans' }, paginationDto),
+      );
+      return plan;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Delete('/:id')
+  @Scopes('delete-plan')
+  @UseGuards(AuthGuard, ScopesGuard)
+  async deleteOne(@Param() planIdDto: PlanIdDto) {
+    try {
+      const plan = await firstValueFrom(
+        this.plansClient.send({ cmd: 'deletePlan' }, planIdDto.id),
       );
       return plan;
     } catch (error) {
