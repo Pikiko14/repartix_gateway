0178454d54f6d42555ee7edeb2fdf4d5d8d5cfc9
Marcos Hernandez <maalhequi95@gmail.com>
2025-03-17 17:31:09 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { RpcCustomExceptionFilter } from './commons/exceptions/rpc-exception.filter';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 const logger = new Logger('RepartiX main gateway');
 
@@ -12,6 +13,18 @@ async function bootstrap() {
 
   // set prefix
   app.setGlobalPrefix('api');
+
+  // enable cors
+  const corsOptions: CorsOptions = {
+    origin: [
+      'http://localhost:9000',
+      'http://localhost:9001'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  };
+  app.enableCors(corsOptions);
 
   // filters
   app.useGlobalFilters(new RpcCustomExceptionFilter());
