e9de687f7ef6e855f05f109f77a65bbede103915
Marcos Hernandez <maalhequi95@gmail.com>
2025-07-11 12:08:08 -0500

    fet: enable update user brand

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -22,6 +22,7 @@ import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { UpdateUserProfileDto } from './dto/update-user-profile.dto';
 import { SubscriptionGuard } from 'src/commons/guards/subscription.guard';
 import { UpdateUserCredentialDto } from './dto/update-user-credential.dto';
+import { UpdateUserBrandDto } from './dto/update-user-brand.dto';
 
 @Controller('users')
 export class UsersController {
@@ -74,6 +75,24 @@ export class UsersController {
       updateUserProfileDto.user_id = req.user.id;
       const user = await firstValueFrom(
         this.client.send('updateUserProfile', updateUserProfileDto),
+      );
+      return user;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Put('update-brand')
+  @Scopes('update-brand')
+  @UseGuards(AuthGuard, ScopesGuard)
+  async updateBrand(
+    @Req() req,
+    @Body() updateUserBrand: UpdateUserBrandDto,
+  ) {
+    try {
+      updateUserBrand.user_id = req.user.parent || req.user.id;
+      const user = await firstValueFrom(
+        this.client.send('updateUserBrand', updateUserBrand),
       );
       return user;
     } catch (error) {
