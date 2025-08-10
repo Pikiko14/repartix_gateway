8d092b5cbb09bed009edb3e819b8b8dc56b63c34
Marcos Hernandez <maalhequi95@gmail.com>
2025-08-10 09:50:49 -0500

    enable list cities

--- src/cities/cities.controller.ts
+++ src/cities/cities.controller.ts
@@ -4,9 +4,19 @@ import { CreateCityDto } from './dto/create-city.dto';
 import { AuthGuard } from 'src/commons/guards/auth.guard';
 import { ScopesGuard } from 'src/commons/guards/scopes.guard';
 import { Scopes } from 'src/commons/decorators/scope.decorator';
+import { QueryParamDto } from 'src/commons/dto/query-params.dto';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { SubscriptionGuard } from 'src/commons/guards/subscription.guard';
-import { Body, Controller, Inject, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 
 @Controller('cities')
 export class CitiesController {
@@ -25,6 +35,22 @@ export class CitiesController {
         this.client.send('create-city', createCityDto),
       );
       return city;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Get()
+  @Scopes('list-city')
+  @UseGuards(AuthGuard, ScopesGuard)
+  async list(@Req() req, @Query() queryParams: QueryParamDto) {
+    queryParams.parent_id = req.user.parent || req.user.id;
+
+    try {
+      const courier = await firstValueFrom(
+        this.client.send('find-all-cities', queryParams),
+      );
+      return courier;
     } catch (error) {
       throw new RpcException(error);
     }
